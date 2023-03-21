0ee8675787ac3049f8c08972b266a3effbbc64a1
Михаил <80469992+TarasovMS@users.noreply.github.com>
2023-03-21 09:53:59 +0400

    temp setup canvas

--- app/src/main/java/com/example/detector/presentation/ui/detectorScreen/DetectorScreen.kt
+++ app/src/main/java/com/example/detector/presentation/ui/detectorScreen/DetectorScreen.kt
@@ -3,10 +3,7 @@ package com.example.detector.presentation.ui.detectorScreen
 import android.graphics.Bitmap
 import android.net.Uri
 import android.view.View
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -24,6 +21,7 @@ import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -38,6 +36,7 @@ import com.example.detector.presentation.ui.detectorScreen.state.DetectorScreenT
 import com.example.detector.presentation.ui.detectorScreen.state.DetectorScreenTriggerEvent.*
 import com.example.detector.presentation.viewmodel.DetectorViewModel
 import com.google.mlkit.vision.face.Face
+import com.google.mlkit.vision.face.FaceLandmark
 
 @Composable
 fun DetectorScreen(viewModel: DetectorViewModel = hiltViewModel()) {
@@ -87,52 +86,70 @@ fun DetectorScreenContent(
     repeatOperationButton: (Bitmap) -> Unit,
     onClickChangePhoto: () -> Unit,
 ) {
+    val modifierForImage = modifier
+        .fillMaxWidth()
+        .height(300.dp)
 
-    Column(
-        modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
+    Box(
+        modifier = modifier
+            .fillMaxSize()
     ) {
 
-        val painter = data.photoBitmap?.let {
-            rememberAsyncImagePainter(it)
-        } ?: painterResource(id = R.drawable.ic_add)
-
-        Image(
-            painter = painter,
+        Column(
             modifier = modifier
-                .height(300.dp)
-                .width(200.dp)
-                .background(colorResource(id = R.color.white))
-                .clickable {
-                    onClickChangePhoto.invoke()
-                },
-            contentDescription = "",
-//            contentScale = if (data.photoBitmap != null) ContentScale.Crop else ContentScale.Fit
-        )
-
-        Button(
-            modifier = modifier
-                .padding(start = 8.dp, end = 8.dp, bottom = 8.dp)
-                .fillMaxWidth()
-                .align(Alignment.CenterHorizontally),
-            onClick = {
-                data.photoBitmap?.let {
-                    repeatOperationButton.invoke(it)
-                }
-            },
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text(text = stringResource(R.string.ok))
-        }
 
-        data.faceList.forEach {
-            it.allContours.map {
-                Text(text = "${it.points} контур ")
+            val painter = data.photoBitmap?.let {
+                rememberAsyncImagePainter(it)
+            } ?: painterResource(id = R.drawable.ic_add)
+
+
+            Image(
+                painter = painter,
+                modifier = modifierForImage
+                    .background(colorResource(id = R.color.white))
+                    .clickable {
+                        onClickChangePhoto.invoke()
+                    },
+                contentDescription = "",
+//            contentScale = if (data.photoBitmap != null) ContentScale.Crop else
+                contentScale = ContentScale.FillWidth
+            )
+
+            Button(
+                modifier = modifier
+                    .padding(start = 8.dp, end = 8.dp, bottom = 8.dp)
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally),
+                onClick = {
+                    data.photoBitmap?.let {
+                        repeatOperationButton.invoke(it)
+                    }
+                },
+            ) {
+                Text(text = stringResource(R.string.ok))
             }
+
+//            data.faceList.forEach {
+//                it.allContours.map {
+//                    Text(text = "${it.points} контур ")
+//                }
+//            }
         }
+    }
 
-
-        if (data.faceList.isNotEmpty()) {
-            DetectorCanvas(faces = data.faceList)
+    if (data.faceList.isNotEmpty()) {
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+        ) {
+            DetectorCanvas(
+                modifier = modifierForImage,
+                faces = data.faceList
+            )
         }
     }
 }
@@ -152,72 +169,156 @@ fun DetectorScreenContentPreview() {
 }
 
 
+@OptIn(ExperimentalTextApi::class)
 @Composable
-fun DetectorCanvas(faces: List<Face>) {
+fun DetectorCanvas(
+    modifier: Modifier = Modifier,
+    faces: List<Face>
+) {
     val view = LocalView.current
 
-    Canvas(modifier = Modifier) {
+    Column(
+    ) {
+        Canvas(modifier = modifier) {
 
-        faces.firstOrNull() { face ->
-            val instaColors = listOf(Color.Yellow, Color.Red, Color.Magenta)
+//            faces.firstOrNull() { face -> //если надо показать одно лицо
+            faces.forEach { face ->
+                val instaColors = listOf(Color.Yellow, Color.Red, Color.Magenta)
 
-            // Draws a circle at the position of the detected face, with the face's track id below.
-            val x: Float = view.translateX(face.boundingBox.centerX())
-            val y: Float = view.translateY(face.boundingBox.centerY())
+                // Draws a circle at the position of the detected face, with the face's track id below.
+                val x: Float = view.translateX(face.boundingBox.centerX())
+                val y: Float = translateY(face.boundingBox.centerY())
 
-//        canvas.drawCircle(
-//            x,
-//            y,
-//            com.google.codelab.mlkit.FaceContourGraphic.FACE_POSITION_RADIUS,
-//            facePositionPaint
-//        )
-//        canvas.drawText(
-//            "id: " + face.getTrackingId(),
-//            x + com.google.codelab.mlkit.FaceContourGraphic.ID_X_OFFSET,
-//            y + com.google.codelab.mlkit.FaceContourGraphic.ID_Y_OFFSET,
-//            idPaint
-//        )
+                drawCircle(
+                    brush = Brush.linearGradient(colors = instaColors),
+                    radius = 10f,
+                    center = Offset(x, y),
+                )
 
-            // Draws a bounding box around the face.
+//                drawText(
+//                    topLeft = Offset(x + -70.0f, y + 80.0f),
+//                    text = "id: " + face.getTrackingId(),
+////
+////                    x + com.google.codelab.mlkit.FaceContourGraphic.ID_X_OFFSET,
+////                    y + com.google.codelab.mlkit.FaceContourGraphic.ID_Y_OFFSET,
+////                    idPaint
+//
+//                )
 
-            // Draws a bounding box around the face.
-            val xOffset: Float = scaleX(face.boundingBox.width() / 2.0f)
-            val yOffset: Float = scaleY(face.boundingBox.height() / 2.0f)
-            val left = x - xOffset
-            val top = y - yOffset
-            val right = x + xOffset
-            val bottom = y + yOffset
+                // Draws a bounding box around the face.
+
+                // Draws a bounding box around the face.
+                val xOffset: Float = scaleX(face.boundingBox.width() / 2.0f)
+                val yOffset: Float = scaleY(face.boundingBox.height() / 2.0f)
+                val left = x - xOffset
+                val top = y - yOffset
+                val right = x + xOffset
+                val bottom = y + yOffset
 
 //            canvas.drawRect(left, top, right, bottom, boxPaint)
 
-            drawRoundRect(
-                brush = Brush.linearGradient(colors = instaColors),
-                cornerRadius = CornerRadius(60f, 60f),
-                style = Stroke(width = 15f, cap = StrokeCap.Round),
-                topLeft = Offset(x = left, y = top),
-            )
+                drawRoundRect(
+                    brush = Brush.linearGradient(colors = instaColors),
+                    cornerRadius = CornerRadius(60f, 60f),
+                    style = Stroke(width = 15f, cap = StrokeCap.Round),
+                    topLeft = Offset(x = x, y = y),
+                )
+//
 
-            drawCircle(
-                brush = Brush.linearGradient(colors = instaColors),
-                radius = 45f,
-                style = Stroke(width = 15f, cap = StrokeCap.Round)
-            )
-            drawCircle(
-                brush = Brush.linearGradient(colors = instaColors),
-                radius = 13f,
-                center = Offset(this.size.width * .80f, this.size.height * 0.20f),
-            )
 
-            true
+//                if (face.smilingProbability != null) {
+//                    canvas.drawText(
+//                        "happiness: " + String.format("%.2f", face.smilingProbability),
+//                        x + com.google.codelab.mlkit.FaceContourGraphic.ID_X_OFFSET * 3,
+//                        y - com.google.codelab.mlkit.FaceContourGraphic.ID_Y_OFFSET,
+//                        idPaint
+//                    )
+//                }
+//
+//                if (face.rightEyeOpenProbability != null) {
+//                    canvas.drawText(
+//                        "right eye: " + String.format("%.2f", face.rightEyeOpenProbability),
+//                        x - com.google.codelab.mlkit.FaceContourGraphic.ID_X_OFFSET,
+//                        y,
+//                        idPaint
+//                    )
+//                }
+//                if (face.leftEyeOpenProbability != null) {
+//                    canvas.drawText(
+//                        "left eye: " + String.format("%.2f", face.leftEyeOpenProbability),
+//                        x + com.google.codelab.mlkit.FaceContourGraphic.ID_X_OFFSET * 6,
+//                        y,
+//                        idPaint
+//                    )
+//                }
+
+                val leftEye = face.getLandmark(FaceLandmark.LEFT_EYE)
+                if (leftEye != null) {
+                    drawCircle(
+                        brush = Brush.linearGradient(colors = instaColors),
+                        radius = 10f,
+                        center = Offset(leftEye.position.x, leftEye.position.y),
+                    )
+                }
+                val rightEye = face.getLandmark(FaceLandmark.RIGHT_EYE)
+                if (rightEye != null) {
+                    drawCircle(
+                        brush = Brush.linearGradient(colors = instaColors),
+                        radius = 10f,
+                        center = Offset(rightEye.position.x, rightEye.position.y),
+                    )
+                }
+
+
+                val contour = face.allContours
+                for (faceContour in contour) {
+                    for (point in faceContour.points) {
+                        val px: Float = view.translateX(point.x.toInt())
+                        val py: Float = translateY(point.y.toInt())
+                        drawCircle(
+                            brush = Brush.linearGradient(colors = instaColors),
+                            radius = 10f,
+                            center = Offset(px, py),
+                        )
+                    }
+                }
+
+
+                val leftCheek = face.getLandmark(FaceLandmark.LEFT_CHEEK)
+                if (leftCheek != null) {
+                    drawCircle(
+                        brush = Brush.linearGradient(colors = instaColors),
+                        radius = 10f,
+                        center = Offset(
+                            view.translateX(leftCheek.position.x.toInt()),
+                            translateY(leftCheek.position.y.toInt())
+                        ),
+                    )
+                }
+                val rightCheek = face.getLandmark(FaceLandmark.RIGHT_CHEEK)
+                if (rightCheek != null) {
+                    drawCircle(
+                        brush = Brush.linearGradient(colors = instaColors),
+                        radius = 10f,
+                        center = Offset(
+                            view.translateX(rightCheek.position.x.toInt()),
+                            translateY(rightCheek.position.y.toInt())
+                        ),
+                    )
+                }
+
+                true
+            }
         }
     }
 }
 
 fun View.translateX(x: Int): Float {
-    return this.width - scaleX(x.toFloat())
+//    return this.width - scaleX(x.toFloat())  //зеркало
+    return scaleX(x.toFloat())
 }
 
-fun View.translateY(y: Int): Float {
+fun translateY(y: Int): Float {
     return scaleY(y.toFloat())
 }
 
